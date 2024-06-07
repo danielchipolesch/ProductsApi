fc7ad3c417f8091ba96ed802780041a83958f7ac
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-06-06 21:44:08 -0300

    Specific method has been created

--- src/main/java/br/com/cotiinformatica/repositories/CategoryRepository.java
+++ src/main/java/br/com/cotiinformatica/repositories/CategoryRepository.java
@@ -4,4 +4,6 @@ import br.com.cotiinformatica.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    boolean existsByName(String name);
 }
